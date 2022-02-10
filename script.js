c7b0c5f26e422ae6c39deb5c59b3e029c7ca97de
Jake Eissler <jakeeissler1@gmail.com>
2022-02-09 21:47:11 -0700

    Beginning to piece together the prompt-alert-confirm format for add all of the characters to the password. Numbers are logged successfuly when the user elects to add them.

--- script.js
+++ script.js
@@ -2,9 +2,11 @@
 var generateBtn = document.querySelector("#generate");
 //character sets for the password as strings
 var numbers = "0123456789";
-var specialChars = "!#$%&'()*+,-./:;<=>?@[]^_`{|}~";
+var specialChars = "!#<=[&'()*>?@+,$%-./`{|:;]^_}~";
 var letters = "abcdefghijklmnopqrstuvwxyz";
 var capLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+//variable for the user password characters left blank
+var userPassword = "";
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -15,3 +17,24 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+function generatePassword() {
+  //keeping the password undefined
+  var newPassword = "";
+
+  // password length prompt
+  var passwordLength = prompt("Length must be between 8 and 128 characters.");
+
+  if (passwordLength < 8 || passwordLength > 128) {
+    alert("Oh no! Make sure you picked a number between 8 and 128!");
+    return;
+  } else if (passwordLength >= 8 && passwordLength <= 128) {
+    var numbersConfirm = confirm("Do you want numbers in the password?");
+
+    if (numbersConfirm) {
+      //if true, numbers will be added to the userPassword
+      userPassword = userPassword + numbers;
+      console.log(userPassword);
+    }
+  }
+}
