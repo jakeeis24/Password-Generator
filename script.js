65fc61970a346669a6096c6e2e17f277705ea27f
Jake Eissler <jakeeissler1@gmail.com>
2022-02-09 21:12:23 -0700

    added the character variables which will be ultimately what creates the password.

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
+//character sets for the password as strings
+var numbers = "0123456789";
+var specialChars = "!#$%&'()*+,-./:;<=>?@[]^_`{|}~";
+var letters = "abcdefghijklmnopqrstuvwxyz";
+var capLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
