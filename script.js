614344768ed6be47aa8102090aed9c88993ecf01
Jake Eissler <jakeeissler1@gmail.com>
2022-02-09 21:07:41 -0700

    restarting on JS. I was completely lost on the first try.

--- script.js
+++ script.js
@@ -1,88 +1,6 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-var lowercase = [
-  "a",
-  "b",
-  "c",
-  "d",
-  "e",
-  "f",
-  "g",
-  "h",
-  "i",
-  "j",
-  "k",
-  "l",
-  "m",
-  "n",
-  "o",
-  "p",
-  "q",
-  "r",
-  "s",
-  "t",
-  "u",
-  "v",
-  "w",
-  "x",
-  "y",
-  "z",
-];
-var uppercase = [
-  "A",
-  "B",
-  "C",
-  "D",
-  "E",
-  "F",
-  "G",
-  "H",
-  "I",
-  "J",
-  "K",
-  "L",
-  "M",
-  "N",
-  "O",
-  "P",
-  "Q",
-  "R",
-  "S",
-  "T",
-  "U",
-  "V",
-  "W",
-  "X",
-  "Y",
-  "Z",
-];
-var specialChars = [
-  "!",
-  "@",
-  "#",
-  "'",
-  "£",
-  "$",
-  "%",
-  "^",
-  "&",
-  "*",
-  "(",
-  ")",
-  "_",
-  "-",
-  "+",
-  "?",
-  "[",
-  "]",
-  "=",
-  "|",
-];
-
-var array1 = [numbers, lowercase, uppercase, specialChars];
-
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -90,15 +8,6 @@ function writePassword() {
 
   passwordText.value = password;
 }
-// create generate password. going to have EVERYTHING in it.
-// USE ARRAYS
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-console.log(lowercase);
-console.log(uppercase);
-
-console.log(numbers);
-console.log(specialChars);
-console.log(array1);
