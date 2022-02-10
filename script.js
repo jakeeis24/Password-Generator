08f64922cf8251c192fbe760d2df26a568791f7a
Jake Eissler <jakeeissler1@gmail.com>
2022-02-09 22:01:45 -0700

    added the remaining confirm and if functions for the remaining variables.

--- script.js
+++ script.js
@@ -29,11 +29,37 @@ function generatePassword() {
     alert("Oh no! Make sure you picked a number between 8 and 128!");
     return;
   } else if (passwordLength >= 8 && passwordLength <= 128) {
+    //does user want numbers included
     var numbersConfirm = confirm("Do you want numbers in the password?");
 
     if (numbersConfirm) {
       //if true, numbers will be added to the userPassword
       userPassword = userPassword + numbers;
+      console.log(userPassword);
+    }
+    //does user want special characters included
+    var symbols = confirm(
+      "Do you want special characters (!@#$...) in the password?"
+    );
+    //if true special characters will be added to the user password
+    if (symbols) {
+      userPassword = userPassword + specialChars;
+      console.log(userPassword);
+    }
+    //does the user want lowercase letters included
+    var lowCaseLetters = confirm(
+      "Do you want lowercase letters in the password?"
+    );
+    //if true lowercase letter will be added to the user password
+    if (lowCaseLetters) {
+      userPassword = userPassword + letters;
+      console.log(userPassword);
+    }
+    //does the user want capital letters included
+    var bigLetters = confirm("Do you want capital letters in the password?");
+    //if true capital letters will be added to the user password
+    if (bigLetters) {
+      userPassword = userPassword + capLetters;
       console.log(userPassword);
     }
   }
