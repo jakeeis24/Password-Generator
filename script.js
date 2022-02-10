ca4d2bd4d0ee9586f762090b2fb689ffc980b8a8
Jake Eissler <jakeeissler1@gmail.com>
2022-02-09 18:24:18 -0700

    added arrays for all of the characters. I also added a 'master' array ~array1~ containing all of the arrays. I am not sure if I will use it yet but it may come in handy.

--- script.js
+++ script.js
@@ -1,20 +1,8 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-}
-// create generate password. going to have EVERYTHING in it.
-// USE ARRAYS
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
-var array1 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-var array2 = [
+var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+var lowercase = [
   "a",
   "b",
   "c",
@@ -42,6 +30,75 @@ var array2 = [
   "y",
   "z",
 ];
+var uppercase = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+var specialChars = [
+  "!",
+  "@",
+  "#",
+  "'",
+  "£",
+  "$",
+  "%",
+  "^",
+  "&",
+  "*",
+  "(",
+  ")",
+  "_",
+  "-",
+  "+",
+  "?",
+  "[",
+  "]",
+  "=",
+  "|",
+];
 
+var array1 = [numbers, lowercase, uppercase, specialChars];
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+}
+// create generate password. going to have EVERYTHING in it.
+// USE ARRAYS
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+console.log(lowercase);
+console.log(uppercase);
+
+console.log(numbers);
+console.log(specialChars);
 console.log(array1);
-console.log(array2);
